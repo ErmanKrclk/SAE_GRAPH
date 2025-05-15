8958b88f6523e11563038301f94377205a7d0bdc
ekr <ekr@kali.com>
2025-05-15 20:43:00 +0200

    exercice 3.3 P1 fini , P2/P3 a faire

--- src/main/java/HollywoodGraphBuilder.java
+++ src/main/java/HollywoodGraphBuilder.java
@@ -61,7 +61,7 @@ public static void exportGraph(Graph<String, DefaultEdge> graph, String chemin) 
 		exporter.setVertexAttributeProvider((x) -> Map.of("label", new DefaultAttribute<>(x, AttributeType.STRING)));
 		exporter.exportGraph(graph, new FileWriter("graph.dot"));
 	}
-
+//3.2
 public static Set<String> collaborateursCommuns(Graph<String, DefaultEdge> graphe, String acteur1, String acteur2) {
     Set<String> voisins1 = Graphs.neighborSetOf(graphe, acteur1);
     Set<String> voisins2 = Graphs.neighborSetOf(graphe, acteur2);
@@ -71,18 +71,73 @@ public static Set<String> collaborateursCommuns(Graph<String, DefaultEdge> graph
     return communs;
 }
 
+//3.3
+/*
+# SAE Exploration algorithmique d'un problème
+
+Voici l'algorithme permettant d'obtenir l'ensemble des collaborateurs à distance k d'un acteur ou d'une actrice. A vous de l'implémenter en JAVA
+
+ Algo collaborateurs_proches(G,u,k):
+    """Algorithme renvoyant l'ensemble des acteurs à distance au plus k de l'acteur u dans le graphe G. La fonction renvoie None si u est absent du graphe.
+    
+    Parametres:
+        G: le graphe
+        u: le sommet de départ
+        k: la distance depuis u
+    """
+    si u n'est pas un sommet de G:
+        afficher u+"est un illustre inconnu"
+        fin de l'algorithme
+    collaborateurs = Ensemble vide
+    Ajouter u à l'ensemble des collaborateurs
+    pour tout i allant de 1 à k:
+        collaborateurs_directs = Ensemble Vide
+        Pour tout collaborateur c dans l'ensemble des collaborateurs
+            Pour tout voisin v de c:
+                si v n'est pas dans l'ensemble des collaborateurs:
+                    Ajouter v à l'ensemble des collaborateurs_directs
+        Remplacer collaborateurs par l'union des collaborateurs et collaborateurs_directs
+    Renvoyer l'ensemble collaborateurs 
+    
+Vous pouvez par exemple utiliser la classe HashSet en java (https://docs.oracle.com/javase/8/docs/api/java/util/HashSet.html) pour les ensembles. */
+
+public static Set<String> collaborateur_proches(Graph<String , DefaultEdge> G, String u, int k){
+    if (!G.containsVertex(u)){
+        System.out.println((u + "est un acteur inconnu"));
+        return null;
+    }
+
+    Set<String> collaborateurs = new HashSet<>();
+    collaborateurs.add(u);
+    for (int i = 1; i <= k ; i++){
+        Set<String> collaborateurs_directs = new HashSet<>();
+        for (String collab : collaborateurs){
+            for (String voisin : Graphs.neighborSetOf(G, collab)){
+                if (!collaborateurs.contains(voisin)){
+                    collaborateurs_directs.add(voisin);
+                }
+            }
+        }
+        collaborateurs.addAll(collaborateurs_directs);
+    }
+    return collaborateurs;
+}
+
+
+
 public static void main(String[] args) {
     try {
-        Graph<String, DefaultEdge> graphe = creerGraphe("data/data_100.txt");
+//3.1
+        Graph<String, DefaultEdge> graphe = creerGraphe("data/data_2.txt");
         System.out.println("Nb acteurs: " + graphe.vertexSet().size());
         System.out.println("Nb liens: " + graphe.edgeSet().size());
 
         exportGraph(graphe, "graph.dot");
         System.out.println("fichier DOT fait");
+//3.2
+        Set<String> communs = collaborateursCommuns(graphe, "Tom Hanks", "Charles Durning");
 
-        Set<String> communs = collaborateursCommuns(graphe, "Tom Hanks", "Meg Ryan");
-
-        System.out.println("Collaborateurs communs entre Tom Hanks et Meg Ryan :");
+        System.out.println("Collaborateurs communs entre Tom Hanks et Charles Durning:");
         if (communs.isEmpty()) {
             System.out.println("Aucun collaborateur commun trouvé.");
         } else {
@@ -90,6 +145,19 @@ public static void main(String[] args) {
                 System.out.println(" - " + nom);
             }
         }
+
+//3.3
+
+        Set<String> proches = collaborateur_proches(graphe, "Brad Pitt", 1);
+        if(proches != null){
+        System.out.println("Collaborateurs proches de Brad Pitt en k=1 : ");
+        for (String nom: proches){
+            if (!nom.equals("Brad Pitt")){
+                System.out.println(" - " + nom);
+            }
+        }
+    }
+        System.out.println(collaborateur_proches(graphe, "Brad Pitt", 2));
 
     } catch (Exception e) { 
         System.out.println("erreur : " + e);
